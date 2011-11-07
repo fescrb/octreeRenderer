c6c8506e1566675e34d8a741c5447ff5ed7ef620
Fernando Escribano <f.escrb@gmail.com>
2011-11-07 15:20:20 +0000

    Added debugging info and ability to work with Intel OpenCL SDK.

--- FindOpenCL.cmake
+++ FindOpenCL.cmake
@@ -10,4 +10,12 @@
 
 find_library(OpenCL_LIBRARIES OpenCL PATHS ENV LD_LIBRARY_PATH)
 
-find_path(OpenCL_INCLUDE_DIR CL/cl.h PATHS $ENV{AMDAPPSDKROOT}/include)
+find_path(OpenCL_INCLUDE_DIR CL/cl.h PATHS $ENV{AMDAPPSDKROOT}/include ENV CPLUS_INCLUDE_PATH)
+
+if(${DEBUG_FINDOPENCL} MATCHES "Yes")
+
+     message("OpenCL_LIBRARIES " ${OpenCL_LIBRARIES})
+
+     message("OpenCL_INCLUDE_DIR " ${OpenCL_INCLUDE_DIR})
+
+endif(${DEBUG_FINDOPENCL} MATCHES "Yes")
