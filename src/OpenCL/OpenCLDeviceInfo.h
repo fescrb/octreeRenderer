d66ee9da7bcb799e4d7b9ab11f65a6460c39deb3
Fernando Escribano Macias <f.escrb@gmail.com>
2011-11-08 19:13:53 +0000

    Created class as an overlay for device info.

---
+++ src/OpenCL/OpenCLDeviceInfo.h
@@ -0,0 +1,15 @@
+#ifndef _OPENCL_DEVICE_INFO_H
+#define _OPENCL_DEVICE_INFO_H
+
+#include <CL/cl.h>
+
+class OpenCLDeviceInfo {
+	public:
+					 OpenCLDeviceInfo(cl_device_id device);
+		virtual 	~OpenCLDeviceInfo();
+
+	private:
+		char		*m_sDeviceName;
+};
+
+#endif //_OPENCL_DEVICE_INFO_H
