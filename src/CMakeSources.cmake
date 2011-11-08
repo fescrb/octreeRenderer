229ba555d8630a5b6ae4d39871b05bb98544575f
Fernando Escribano <f.escrb@gmail.com>
2011-11-08 10:20:52 +0000

    Added OCLUtils to build and added some more error messages.

--- src/CMakeSources.cmake
+++ src/CMakeSources.cmake
@@ -1,7 +1,9 @@
 set(sources Device.cpp
 			OpenCLContext.cpp
+			OpenCLUtils.cpp
 			main.cpp )
 
 set(headers Context.h
 			OpenCLContext.h
+			OpenCLUtils.h
 			Device.h )
--- src/OpenCLUtils.cpp
+++ src/OpenCLUtils.cpp
@@ -6,6 +6,18 @@ const char* errorToCString(cl_int error_number) {
 			return "CL_SUCCESS";
 		case CL_DEVICE_NOT_FOUND:
 			return "CL_DEVICE_NOT_FOUND";
+		case CL_DEVICE_NOT_AVAILABLE:
+			return "CL_DEVICE_NOT_AVAILABLE";
+		case CL_COMPILER_NOT_AVAILABLE:
+			return "CL_COMPILER_NOT_AVAILABLE";
+		case CL_MEM_OBJECT_ALLOCATION_FAILURE:
+			return "CL_MEM_OBJECT_ALLOCATION_FAILURE";
+		case CL_OUT_OF_RESOURCES:
+			return "CL_OUT_OF_RESOURCES";
+		case CL_OUT_OF_HOST_MEMORY:
+			return "CL_OUT_OF_HOST_MEMORY";
+		case CL_PROFILING_INFO_NOT_AVAILABLE:
+			return "CL_PROFILING_INFO_NOT_AVAILABLE";
 		default:
 			return "Error number invalid or unknown.";
 	}
