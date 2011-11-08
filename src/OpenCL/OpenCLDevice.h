99847983d1beb16d70261b096605e5959c164bea
Fernando Escribano Macias <f.escrb@gmail.com>
2011-11-08 18:19:04 +0000

    More work on OpenCLDevice.

--- src/OpenCL/OpenCLDevice.h
+++ src/OpenCL/OpenCLDevice.h
@@ -1,6 +1,19 @@
 #ifndef _OPENCL_DEVICE_H
 #define _OPENCL_DEVICE_H
 
-class OpenCLDevice;
+#include "Device.h"
+#include <CL/cl.h>
+
+class OpenCLDevice:
+	public Device{
+
+	public:
+							 OpenCLDevice(cl_device_id);
+		virtual 			~OpenCLDevice();
+
+	private:
+
+
+};
 
 #endif //_OPENCL_DEVICE_H
