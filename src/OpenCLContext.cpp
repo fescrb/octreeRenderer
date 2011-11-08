aac2966458bf97b72220eb68ab4c98a5cd374bfd
Fernando Escribano <f.escrb@gmail.com>
2011-11-08 10:46:20 +0000

    OpenCLContext now uses OpenCLUtils to print errors.

--- src/OpenCLContext.cpp
+++ src/OpenCLContext.cpp
@@ -1,13 +1,15 @@
 #include "OpenCLContext.h"
+#include "OpenCLUtils.h"
 
 #include <stdio.h>
 
 OpenCLContext::OpenCLContext() {
 	cl_uint num_of_platforms = 0;
+	cl_platform_id plat;
 	// We get the number of platforms.
-	clGetPlatformIDs(0, NULL, &num_of_platforms);
+	cl_int err = clGetPlatformIDs(1, &plat, &num_of_platforms);
 
-	printf("There are %d platforms.\n", num_of_platforms);
+	printf("There are %d platforms, with error %s.\n", num_of_platforms, clErrorToCString(err));
 }
 
 OpenCLContext::~OpenCLContext() {
