dfc46436538a0465fa233c30d938edf742862ea8
Fernando Escribano <f.escrb@gmail.com>
2012-02-10 19:22:40 +0000

    Find OpenCL in OSX.

--- FindOpenCL.cmake
+++ FindOpenCL.cmake
@@ -8,9 +8,17 @@
 #  OpenCL_INCLUDE_DIR   - The location of the OpenCL headers.
 #  OpenCL_LIBRARIES     - A list containing the path of the OpenCL libraries.
 
-find_library(OpenCL_LIBRARIES OpenCL PATHS ENV LD_LIBRARY_PATH)
+if(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+    find_library(OpenCL_LIBRARIES OpenCL PATHS ENV LD_LIBRARY_PATH)
 
-find_path(OpenCL_INCLUDE_DIR CL/cl.h PATHS $ENV{AMDAPPSDKROOT}/include ENV CPLUS_INCLUDE_PATH)
+    find_path(OpenCL_INCLUDE_DIR CL/cl.h PATHS $ENV{AMDAPPSDKROOT}/include ENV CPLUS_INCLUDE_PATH)
+endif(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+
+if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+    find_library(OpenCL_LIBRARIES OpenCL)
+
+    find_path(OpenCL_INCLUDE_DIR OpenCL/cl.h)
+endif(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
 
 if(${OpenCL_LIBRARIES} MATCHES "OpenCL_LIBRARIES-NOTFOUND" OR ${OpenCL_INCLUDE_DIR} MATCHES "OpenCL_INCLUDE_DIR-NOTFOUND")
      set(OpenCL_FOUND "No")
