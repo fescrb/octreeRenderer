54119d41494675afbc9b3192c94c643b856bd3c1
Fernando Escribano <f.escrb@gmail.com>
2012-02-10 11:47:03 +0000

    Much work on ray tracing.

--- src/Test/TestDevice.cpp
+++ src/Test/TestDevice.cpp
@@ -28,14 +28,32 @@ bool noChildren(char* node) {
 	return !node[0];
 }
 
-bool nodeHasChildAt(float3 rayPos, float3 nodeCentre, char* node) {
+char makeXYZFlag(float3 rayPos, float3 nodeCentre) {
 	float3 flagVector = rayPos - nodeCentre;
-	int flag = 0;
+	char flag = 0;
 	for(int i = 0; i < 3; i++)
 		if(flagVector[3] >= 0)
 			flag | (1 << i);
-	
-	return node[0] & (1 << flag);  
+		
+	return flag;
+}
+
+char makeChildFlag(float3 rayPos, float3 nodeCentre) {
+	return 0  | (1 << makeXYZFlag(rayPos, nodeCentre));
+}
+
+bool nodeHasChildAt(float3 rayPos, float3 nodeCentre, char* node) {
+	return node[0] & makeChildFlag(rayPos,nodeCentre);  
+}
+
+char* getChild(float3 rayPos, float3 nodeCentre, char* node) {
+	int counter = 0;
+	bool found = false;
+	char flag = makeChildFlag(rayPos, nodeCentre);
+	for(int i = 0; i <= flag; i++) 
+		if(node[0] & (1 << i))
+			counter++;
+	return node + (counter*4);
 }
 
 TestDevice::TestDevice()
@@ -58,9 +76,20 @@ void TestDevice::sendData(char* data) {
 }
 
 struct Stack {
+	char* address;
 	float3 far_corner, node_centre;
 	float t_min, t_max;
 };
+
+// Returns new index.
+int push(Stack* stack, int index, char* node, float3 far_corner, float3 node_centre, int t_min, int t_max){
+	stack[index].address = node;
+	stack[index].far_corner = far_corner;
+	stack[index].node_centre = node_centre;
+	stack[index].t_min = t_min;
+	stack[index].t_max = t_max;
+	return index+1;
+}
 
 void TestDevice::render(RenderInfo &info) {
 	if(!m_pFrame) {
@@ -101,11 +130,8 @@ void TestDevice::render(RenderInfo &info) {
 							  
 			float3 corner_close(corner_far.neg());
 
-			float3 vt0 = (corner_close - o) / d;
-			float3 vt1 = (corner_far - o) / d;
-
-			float t_min = max(vt0);
-			float t_max = min(vt1);
+			float t_min = max((corner_close - o) / d);
+			float t_max = min((corner_far - o) / d);
 			
 			// If we are out 
 			if(t < t_min)
@@ -115,6 +141,7 @@ void TestDevice::render(RenderInfo &info) {
 			float3 voxelCentre(0.0f, 0.0f, 0.0f);
 			bool collission = false;	
 			int curr_index = 0;
+			Stack stack[info.maxOctreeDepth - 1];
 			
 			// We are out of the volume and we will never get to it.
 			
@@ -130,13 +157,52 @@ void TestDevice::render(RenderInfo &info) {
 				if(noChildren(curr_address)){
 					collission = true;
 				} else {
-					//bool node_has
-					
-					vt0 = (corner_close - o) / d;
-					vt1 = (corner_far - o) / d;
-					
-					float t_min = max(vt0);
-					float t_max = min(vt1);
+					// If we are inside the node
+					if(t_min <= t && t < t_max) {
+						float3 rayPos = o + (d * t);
+						
+						char xyz_flag = makeXYZFlag(rayPos, voxelCentre);
+						float nodeHalfSize = fabs((corner_far-voxelCentre)[0])/2.0f;
+						
+						float3 tmpNodeCentre( xyz_flag & 1 ? voxelCentre[0] + nodeHalfSize : voxelCentre[0] - nodeHalfSize,
+											  xyz_flag & 2 ? voxelCentre[1] + nodeHalfSize : voxelCentre[1] - nodeHalfSize,
+											  xyz_flag & 4 ? voxelCentre[2] + nodeHalfSize : voxelCentre[2] - nodeHalfSize);
+						
+						float3 tmp_corner_far(d[0] >= 0 ? tmpNodeCentre[0] + nodeHalfSize : tmpNodeCentre[0] - nodeHalfSize,
+											  d[1] >= 0 ? tmpNodeCentre[1] + nodeHalfSize : tmpNodeCentre[1] - nodeHalfSize,
+											  d[2] >= 0 ? tmpNodeCentre[2] + nodeHalfSize : tmpNodeCentre[2] - nodeHalfSize);
+						
+						float tmp_max = min((corner_far - rayPos) / d);
+						
+						if(nodeHasChildAt(rayPos, voxelCentre, curr_address)) {
+							// If the voxel we are at is not empty, go down.
+							
+							curr_index = push(stack, curr_index, curr_address, corner_far, voxelCentre, t_min, t_max);
+							
+							corner_far = tmp_corner_far;
+							voxelCentre = tmpNodeCentre;
+							corner_close =  float3(d[0] >= 0 ? voxelCentre[0] - nodeHalfSize : voxelCentre[0] + nodeHalfSize,
+												   d[1] >= 0 ? voxelCentre[1] - nodeHalfSize : voxelCentre[1] + nodeHalfSize,
+												   d[2] >= 0 ? voxelCentre[2] - nodeHalfSize : voxelCentre[2] + nodeHalfSize);
+							t_max = tmp_max;
+							t_min = max((corner_close - rayPos) / d);
+							
+						} else {
+							// If the child is empty, we step the ray.
+							t = tmp_max;
+						}
+					} else {
+						// We are outside the node. Pop the stack
+						curr_index--;
+						if(curr_index>=0) {
+							// Pop that stack!
+							corner_far = stack[curr_index].far_corner;
+						} else {
+							// We are outside the volume.
+							curr_address = 0;
+							collission = true;
+						}
+					}
 				}
 			}
 			
