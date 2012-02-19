da4718e627ca32e3f13b116bcb92cd1141bfa474
Fernando Escribano <f.escrb@gmail.com>
2012-02-19 12:53:27 +0000

    Remove unused files.

--- src/Graphics/Cocoa/CocoaGLSurface.mm
+++
@@ -1,9 +0,0 @@
-//
-//  CocoaGLSurface.mm
-//  octreeRenderer
-//
-//  Created by Nataliya Dubrovska on 19/02/2012.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
-#include <iostream>
