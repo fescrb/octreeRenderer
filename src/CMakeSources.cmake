ff472c58ada1d6621a526cf442fb2472648a96f5
Fernando Escribano <f.escrb@gmail.com>
2012-02-22 16:32:45 +0000

    Restructuring of code base.

--- src/CMakeSources.cmake
+++ src/CMakeSources.cmake
@@ -1,25 +1,19 @@
 set(sources ${sources}
-			Image.cpp
 			DeviceInfo.cpp
 			Device.cpp
 			DeviceManager.cpp
 			DataManager.cpp
-            SourceFile.cpp
-            SourceFileManager.cpp
             ProgramState.cpp
             Window.cpp
 			main.cpp )
 
 set(headers ${headers}
-			Image.h
 			RenderInfo.h
 			DeviceInfo.h
 			Device.h 
 			Context.h
 			DeviceManager.h
 			DataManager.h
-            SourceFile.h
-            SourceFileManager.h
             ProgramState.h
             Window.h
 			)
