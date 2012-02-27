d2da257d70fdcb218bfd8edaacc0cbdeb039b5b4
Fernando Escribano <f.escrb@gmail.com>
2012-02-27 19:26:10 +0000

    Work on OBJFileReader

---
+++ src/IO/OBJFileReader.cpp
@@ -0,0 +1,64 @@
+#include "OBJFileReader.h"
+
+#include <cstring>
+#include <fstream>
+#include <cmath>
+
+OBJFileReader::OBJFileReader(const char* filename) {
+    m_filename = (char*)malloc(strlen(filename)+1);
+}
+        
+mesh OBJFileReader::getMesh() {
+    std::ifstream in;
+    in.open(m_filename);
+    
+    mesh objMesh;
+    std::vector<float4> vertexList;
+    std::vector<float4> normalList;
+    int normalCounter = 0;
+    
+    while(!in.eof()) {
+        char line[1024];
+
+        in.getline(line, 1024);
+        
+        switch(getLineType(line)){
+            case TYPE_VERTEX_DECLARATION:
+                vertexList.push_back(getVertexFromLine(line));
+            case TYPE_NORMAL_DECLARATION:
+                float4 normal = getVertexFromLine(line);
+                normal.setW(0.0f);
+                normalList.push_back(normal);
+            case TYPE_FACE_DECLARATION:
+                ;
+            default:
+                ; // We do nothing.
+        }
+    }
+    
+    return objMesh;
+}
+
+OBJFileReader::LineType OBJFileReader::getLineType(const char* line) {
+    switch(line[0]) {
+        case '#':
+            return TYPE_COMMENT;
+        case 'v':
+            if(line[1] = ' ')
+                return TYPE_VERTEX_DECLARATION;
+            if(line[1] = 'n')
+                return TYPE_NORMAL_DECLARATION;
+        case 'f':
+            return TYPE_FACE_DECLARATION;
+        default:
+            return TYPE_UNKOWN;
+    }
+}
+
+float4 OBJFileReader::getVertexFromLine(char* line) {
+    strtok(line, " ");
+    char* x = strtok(NULL, " ");
+    char* y = strtok(NULL, " ");
+    char* z = strtok(NULL, " ");
+    retutn float4(atof(x),atof(y),atof(z),1.0f);
+}
--- src/IO/OBJFileReader.h
+++ src/IO/OBJFileReader.h
@@ -1,8 +1,28 @@
 #ifndef _OBJ_FILE_READER_H
 #define _OBJ_FILE_READER_H
 
+#include "Mesh.h"
+
 class OBJFileReader {
-    
+    public:
+        explicit                 OBJFileReader(const char* filename);
+        
+        mesh                     getMesh();
+        
+        enum                     LineType {
+            TYPE_COMMENT, 
+            TYPE_VERTEX_DECLARATION,
+            TYPE_NORMAL_DECLARATION,
+            TYPE_FACE_DECLARATION,
+            TYPE_UNKOWN
+        };
+        
+        LineType                 getLineType(const char* line);
+        
+        float4                   getVertexFromLine(char* line);
+        
+    private:
+        char*                    m_filename;
 };
 
 #endif _OBJ_FILE_READER_H
