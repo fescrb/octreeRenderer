82d302c74e3802b333f5e8666c693027a35e8d7a
Fernando Escribano <f.escrb@gmail.com>
2012-02-28 15:47:36 +0000

    SourceFile classes moved back to Common due to dependencies.

--- src/Common/CMakeSources.cmake
+++ src/Common/CMakeSources.cmake
@@ -2,10 +2,14 @@ set(this_sources 	${this_sources}
 					Image.cpp
 					DebugUtils.cpp
 					HighResTimer.cpp
+                    SourceFile.cpp
+                    SourceFileManager.cpp
 					 )
 
 set(this_headers 	${this_headers}
 					Image.h
 					DebugUtils.h
 					HighResTimer.h
+                    SourceFile.h
+                    SourceFileManager.h
 					 )
--- src/IO/CMakeSources.cmake
+++ src/IO/CMakeSources.cmake
@@ -1,11 +1,7 @@
 set(this_sources 	${this_sources}
                     OBJFileReader.cpp
-					SourceFile.cpp
-					SourceFileManager.cpp
 					 )
 
 set(this_headers 	${this_headers}
                     OBJFileReader.h
-                    SourceFile.h
-					SourceFileManager.h
 					 )
